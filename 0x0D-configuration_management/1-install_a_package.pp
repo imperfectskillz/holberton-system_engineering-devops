c3325c30f70bea73e98fc5a26ac91d65ef158ad1
imperfectskillz <j.choi.89@gmail.com>
2018-03-28 14:33:14 +0000

    comments

--- 0x0D-configuration_management/1-install_a_package.pp
+++ 0x0D-configuration_management/1-install_a_package.pp
@@ -1,3 +1,4 @@
+# installs puppet lint
 package { 'puppet-lint':
   ensure   => '2.1.1',
   provider => 'gem',
--- 0x0D-configuration_management/2-execute_a_command.pp
+++ 0x0D-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# kills killmenow process
 exec { 'killmenow':
   path    => ['/usr/bin', '/usr/sbin'],
   command => 'pkill -f killmenow'
