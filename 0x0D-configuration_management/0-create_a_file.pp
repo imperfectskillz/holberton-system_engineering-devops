0932e009a1b59e60b84e49ab12a7ab9d89e5ce69
imperfectskillz <j.choi.89@gmail.com>
2018-03-28 14:15:13 +0000

    lint

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file { '/tmp/holberton':
-      ensure     => file,
-      path       => '/tmp/holberton',
-      mode       => '0744',
-      owner      => 'www-data',
-      group      => 'www-data',
-      content    => 'I love Puppet',
+      ensure  => file,
+      path    => '/tmp/holberton',
+      mode    => '0744',
+      owner   => 'www-data',
+      group   => 'www-data',
+      content => 'I love Puppet',
 }
