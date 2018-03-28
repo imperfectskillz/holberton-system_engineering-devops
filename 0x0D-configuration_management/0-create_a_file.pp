04b6ce86541e43a3b8728c143788df03eaa55503
imperfectskillz <j.choi.89@gmail.com>
2018-03-28 14:10:59 +0000

    changed to root

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/holberton':
       ensure     => file,
       path       => '/tmp/holberton',
-      permission => '0744',
+      mode       => '0744',
       owner      => 'www-data',
       group      => 'www-data',
       content    => 'I love Puppet',
