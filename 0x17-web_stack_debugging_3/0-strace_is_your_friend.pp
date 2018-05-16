2f374f40aa21afd413fd95cbb9dc979192b4f72f
imperfectskillz <j.choi.89@gmail.com>
2018-05-16 02:27:17 +0000

    path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { "replace phpp to php":
-     command => "bin/sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+     command => "/bin/sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
      provider => "shell",
 }
