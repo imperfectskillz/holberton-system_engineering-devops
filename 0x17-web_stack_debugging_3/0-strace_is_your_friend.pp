8ba96dd3575a860a4cb4f2d4f9377d86757da942
imperfectskillz <j.choi.89@gmail.com>
2018-05-16 03:29:10 +0000

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { "replace phpp to php":
-     command => "/bin/sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-     provider => "shell",
+# using puppet to change php to phpp
+exec { 'replace phpp to php':
+  command  => "/bin/sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  provider => 'shell',
 }
