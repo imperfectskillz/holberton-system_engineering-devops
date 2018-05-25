1f63dcc078323d5083a55e33020c72a69863474f
imperfectskillz <j.choi.89@gmail.com>
2018-05-25 05:33:00 +0000

    two commands

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,5 @@
 # raise ulimit using sed
 exec { 'debug':
-  command  => 'sed -i "s/15/5000/g" /etc/default/nginx',
-  provider => 'shell'
-  path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-}
-
-exec { 'restart':
-  command  => 'sudo service nginx restart'
+  command  => 'sed -i "s/15/5000/g" /etc/default/nginx; sudo service nginx restart',
   provider => 'shell',
 }
