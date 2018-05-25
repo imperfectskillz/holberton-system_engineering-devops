3a712535b5fd78f384dc8096acb03e9226893e6e
imperfectskillz <j.choi.89@gmail.com>
2018-05-25 05:27:42 +0000

    sudo???

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,10 @@
 # raise ulimit using sed
 exec { 'debug':
   command  => 'sed -i "s/15/5000/g" /etc/default/nginx',
-  command  => 'sudo service nginx restart'
   provider => 'shell'
 }
 
 exec { 'restart':
-  command  => 'service nginx restart'
+  command  => 'sudo service nginx restart'
   provider => 'shell',
 }
