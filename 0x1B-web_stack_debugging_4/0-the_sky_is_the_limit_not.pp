4391a995f32ce46a4e3b66f5088572df1b651a06
imperfectskillz <j.choi.89@gmail.com>
2018-05-25 05:30:02 +0000

    added path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,7 @@
 exec { 'debug':
   command  => 'sed -i "s/15/5000/g" /etc/default/nginx',
   provider => 'shell'
+  path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 
 exec { 'restart':
